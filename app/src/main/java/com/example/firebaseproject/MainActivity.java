6046d28202af1c0dc5cf4b3d7534f1dc21dbc410
kanchon136 <miakanchon5100@gmail.com>
2021-02-23 20:51:08 +0600

    Initialized

---
+++ app/src/main/java/com/example/firebaseproject/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.firebaseproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Object data=FirebaseDatabase.getInstance();
+    }
+}
