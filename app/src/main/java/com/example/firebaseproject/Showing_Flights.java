a68db1ad8ac221390b00cec81c40e7ef37080c3a
kanchon136 <miakanchon5100@gmail.com>
2021-02-24 12:02:42 +0600

    admin layout added

---
+++ app/src/main/java/com/example/firebaseproject/Showing_Flights.java
@@ -0,0 +1,14 @@
+package com.example.firebaseproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Showing_Flights extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showing__flights);
+    }
+}
